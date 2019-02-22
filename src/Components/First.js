f761ac335535b91de2e30e3793c8c934b91d7a06
Lucas D <l.dupias@net.estia.fr>
2019-02-22 16:19:28 +0100

    first commit

---
+++ src/Components/First.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+import Button from '@material-ui/core/Button';
+
+
+
+class First extends React.Component {
+    render() {
+        return ( 
+            <div>
+            
+                <Button color="primary" >
+                  OK
+                </Button>
+              
+          </div>
+          );
+    }
+}
+
+export default First;
---
+++ src/Components/Layout/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import SignedInLinks from './SignedInLinks'
+
+const Navbar = () => {
+ return (
+     
+     <nav className="nav.nav-wrapper.grey.darken-3">
+     <div className="container">
+    
+ <Link to='/' className="brand-logo" >Lucas' Page</Link>  
+ <SignedInLinks/>
+ </div>
+     </nav>
+
+     
+
+     
+ )
+}
+export default Navbar;
